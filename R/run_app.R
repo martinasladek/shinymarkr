135ac79278f06f3bbd4bb276b8d3ba752d8bdeed
martinasladek <m.sladekova@sussex.ac.uk>
2024-09-02 14:34:20 +0100

    commit initial setup

--- R/run_app.R
+++ R/run_app.R
@@ -1,6 +1,5 @@
 #' Run ShinyMarkR app
 #'
-#' @param ...
 #'
 #' @return Loads the app
 #' @export
@@ -10,7 +9,13 @@
 #'  run_app()
 #' }
 #'
-run_app <- function(...){
+run_app <- function(){
+
+  if(dir.exists(file.path(here::here("shiny_markR")))) {
+    cat("dir exists")
+  } else {
+    cat("dir does not exist")
+  }
 
   # Define UI for application
   ui <- shinydashboard::dashboardPage(
